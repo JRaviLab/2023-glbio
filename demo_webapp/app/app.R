1249bf6c9d92b6878ee452224260ab9ddbf11873
Faisal Alquaddoomi <faisal.alquaddoomi@cuanschutz.edu>
2023-05-14 22:16:12 -0600

    Many, many changes: histogram rendering, multiple default inputs from the iprscanr package, fixes, etc.

--- demo_webapp/app/app.R
+++ demo_webapp/app/app.R
@@ -7,6 +7,8 @@
 # load up some required libraries, notably shiny and some other bits for
 # rendering datatables and async processing
 library(shiny)
+library(shinyjs)
+library(shinycssloaders)
 library(DT)
 library(promises)
 library(future)
@@ -28,82 +30,30 @@ USER_EMAIL <- Sys.getenv("USER_EMAIL")
 # set future to use multisession mode
 plan(multisession)
 
-# supply some default text to fill in the box when "load example" is pressed
-DEFAULT_FASTA_TEXT <-
-    ">ANY95992.1 protein LiaH [Bacillus altitudinis]
-MVLRRVRDMFVATVNEGLDKLENPRVMLNQYVRDMEDDIAKAKHAIIKQQTIQQGFL
-RKAEETEAFADKRKKQAELAFQAGEEELVRKALTEMKYFEEKHNEYQEAYQQSVKQL
-KELKEQLQHLETKLRDVKDKKQALIARANAAQAKQHMNESMNKVDSESAYKEFLRM
-ENRIEEMETKAGSYAQFADQGAYAHLDYADEVEKEWQKLQRSKQPEKQPAN"
+# supply some default text to fill in the box when "load example" buttons are pressed
+single_fasta_example <- system.file(package = "iprscanr", "extdata", "ex-in-CAA75348.1.faa")
+multi_fasta_example <- system.file(package = "iprscanr", "extdata", "ex-in-2.faa")
 
 # helper function for writing to stderr, where it'll be visible from the server log
 console_stderr <- function(x) {
     cat(file = stderr(), x, "\n")
 }
 
-# main worker function that submits a string of FASTA data to InterProScan via
-# the iprscanr package, and returns the resulting classifications as a dataframe
-get_input_and_submit_ipr <- function(input_fasta) {
-    # put the input into a temp file
-    tmp_fasta_in <- tempfile()
-    write(input_fasta, tmp_fasta_in)
-
-    # create a destination for the output, too
-    tmp_results_out <- tempdir()
-
-    # start the submission, recording the time it took to run
-    start_time <- Sys.time()
-    results_folder <- submit_ipr(tmp_fasta_in, tmp_results_out, USER_EMAIL)
-    end_time <- Sys.time()
-
-    # get total duration as human readable duration, e.g. MM:SS
-    dur <- lubridate::as.period(end_time - start_time)
-    total_time <- sprintf(
-        '%02d:%02d',
-        as.integer(lubridate::minute(dur)), as.integer(lubridate::second(dur))
-    )
-
-    # find and return a list of every TSV in the results folder
-    results_files <- list.files(results_folder, pattern = "*.tsv", full.names = TRUE)
-
-    # read each TSV into a dataframe, annotated with header info
-    results_dfs <- lapply(results_files, function(tsv_path) {
-        df <- read.table(
-            tsv_path, sep = "\t", header = FALSE, stringsAsFactors = FALSE,
-            col.names = c(
-                "Protein accession",
-                "Sequence MD5 digest",
-                "Sequence length",
-                "Analysis",
-                "Signature accession",
-                "Signature description",
-                "Start Loc", "Stop Loc",
-                "Score", "Status", "Date",
-                "IPR Accession", "IPR Description"
-            )
-        )
-
-        return(list(filename=tsv_path, data=df))
-    })
-
-    # create results object w/status and dataframes
-    return(
-        list(
-            "status"=list(
-                "total_time"=total_time,
-                "results_folder"=results_folder
-            ),
-            "dataframes"=results_dfs
-        )
-    )
+setControlStatus <- function(is_enabled) {
+    set_func <- ifelse(is_enabled, enable, disable)
+    set_func("interpro_input")
+    set_func("interpro_submit")
+    set_func("load_single_example")
+    set_func("load_multi_example")
 }
-
 
 # =============================================================================
 # === UI definition
 # =============================================================================
 
 ui <- fluidPage(
+    useShinyjs(),
+
     # Application title
     titlePanel("iprscanr Demo App"),
 
@@ -113,23 +63,38 @@ ui <- fluidPage(
             textAreaInput("interpro_input", "Enter FASTA data", rows = 10),
 
             fluidRow(
-                actionButton("interpro_submit", "Run InterProScan"),
-                actionButton("load_example", "Load Example FASTA")
-            ),
+                actionButton("interpro_submit", "Run InterProScan", class = "btn-success"),
+                actionButton("load_single_example", "Load Example Single-Sequence FASTA"),
+                actionButton("load_multi_example", "Load Example Multi-Sequence FASTA")
+            , style="margin-left: 0px;"),
 
-            width=5
+            width=12
         ),
 
         # display resulting tables from interproscan
         mainPanel(
-            # shinycssloaders::withSpinner(
-            #     textOutput("status_out")
-            # ),
+            div(
+                # textOutput("status_out"),
+                htmlOutput(outputId = 'status_out')
+            , style="margin-bottom: 20px;"),
 
-            textOutput("status_out"),
-            uiOutput("results_dts"),
+            tabsetPanel(
+                tabPanel("Results", 
+                    withSpinner(
+                        DT::dataTableOutput("ipr_results")
+                    ),
+                    style="padding: 20px; overflow-x: scroll;"
+                ),
+                tabPanel("Runtime Info",
+                    withSpinner(
+                        plotOutput("runtime_plot")
+                        # uiOutput("runtime_output")
+                    ),
+                    style="padding: 20px;"
+                )
+            ),
 
-            width=7
+            width=12
         )
     )
 )
@@ -139,72 +104,122 @@ ui <- fluidPage(
 # === server logic
 # =============================================================================
 
+# main worker function that submits a string of FASTA data to InterProScan via
+# the iprscanr package, and returns the resulting classifications as a dataframe
+get_input_and_submit_ipr <- function(input_fasta) {
+    # ensure our output makes it to stderr
+    if (!interactive()) sink(stderr(), type = "output")
+
+    # put the input into a temp file, and output into a temp directory
+    tmp_fasta_in <- tempfile(); write(input_fasta, tmp_fasta_in)
+    tmp_out_dir <- tempdir()
+
+    # start the submission, recording the time it took to run
+    start_time <- Sys.time()
+    ipr_results <- submit_ipr(tmp_fasta_in, tmp_out_dir, USER_EMAIL)
+    end_time <- Sys.time()
+
+    console_stderr("Final Output:")
+    console_stderr(capture.output(dput(ipr_results)))
+
+    # get total duration as human readable duration, e.g. MM:SS
+    dur <- lubridate::as.period(end_time - start_time)
+    total_time <- sprintf('%02d:%02d', as.integer(lubridate::minute(dur)), as.integer(lubridate::second(dur)))
+
+    # create results object w/status and data
+    return(
+        list(
+            "status"=list(
+                "total_time"=total_time,
+                "job_dir"=tmp_out_dir # used to fetch the job runtime histogram
+            ),
+            "data"=ipr_results
+        )
+    )
+}
+
 server <- function(input, output) {
-    results <- reactiveValues()
-    results$status <- NULL
-    results$dataframes <- NULL
+    output$ipr_results <- NULL
+    output$runtime_plot <- NULL
+    output$status_out <- renderText({ "" })
+
+    hist_values <- reactiveValues()
+    hist_values$data <- NULL
+    hist_values$job_dir <- NULL
 
     # when the submit button is pressed, submit the input to interproscan
-    observeEvent(input$interpro_submit,
-        {
-            start_msg <- "Running InterProScan..."
-            console_stderr(start_msg)
-            output$status_out <- renderText(start_msg)
+    observeEvent(input$interpro_submit, {
+        setControlStatus(FALSE)
 
-            submitted_fasta <- isolate(input$interpro_input)
+        # reset the histogram plot
+        hist_values$data <- NULL
+        hist_values$job_dir <- NULL
 
+        start_msg <- "Running InterProScan (this may take a minute or two)..."
+        console_stderr(start_msg)
+        shinyjs::html(id = 'status_out', html = start_msg)
+
+        submitted_fasta <- isolate(input$interpro_input)
+
+        output$ipr_results <- renderDT({
             future_promise({
-                return(get_input_and_submit_ipr(submitted_fasta))
+                get_input_and_submit_ipr(submitted_fasta)
             }) %...>% (function(promise_results) {
-                # write out debug output
-                console_stderr(paste(promise_results))
-
-                # format duration in minutes as a human-readable duration
-                duration <- promise_results$status$total_time
-
                 # write out a success message
-                complete_msg <- paste0("Complete! Took ", promise_results$status$total_time)
+                complete_msg <- paste0("Complete! Runtime: ", promise_results$status$total_time)
                 console_stderr(complete_msg)
-                output$status_out <- renderText(complete_msg)
+                shinyjs::html(id = 'status_out', html = complete_msg)
 
-                # and finally update the reactive value that'll render the dataframes
-                results$status <- promise_results$status
-                results$dataframes <- promise_results$dataframes
+                # set the histogram plot and other relevant bits, too
+                hist_values$data <- promise_results$hist_plot
+                hist_values$job_dir <- promise_results$status$job_dir
+
+                # re-renable the submit button
+                setControlStatus(TRUE)
+
+                promise_results$data
+            }) %...T!% (function(err) {
+                shinyjs::html(id = 'status_out', html = toString(err))
+                setControlStatus(TRUE)
             })
-        },
-        ignoreInit = TRUE,
-        ignoreNULL = TRUE
-    )
+        })
+    }, ignoreInit = TRUE)
 
-    # render the set of dataframes returned from ipr_submit as DT datatables
-    output$results_dts <- renderUI({
-        df_list = tagList()
+    output$runtime_plot <- renderPlot({
+        result <- NULL
 
-        console_stderr("In output$results_dts <- renderUI(...)")
-        console_stderr(paste(results$dataframes))
+        tryCatch({
+            result <- job_time_hist(file.path(hist_values$job_dir, "api.log"))
+        }, error = function(e) {
+            console_stderr("Error generating runtime plot:")
+            console_stderr(toString(e))
+            result <- FALSE
+        })
 
-        if (!is.null(results$dataframes) && length(results$dataframes) > 0) {
-            for (idx in 1:length(results$dataframes)) {
-                cur_elem <- results$dataframes[[idx]]
-                console_stderr(paste0("* Processing IDX [[", idx, "]]] = ", paste(cur_elem), "\n"))
-
-                df_list[[idx]] = tagList()
-                df_list[[idx]][[1]] = h3(paste0("Table ", cur_elem$filename))
-                df_list[[idx]][[2]] <- DT::renderDataTable(cur_elem$data)
-            }
-        }
-
-        # df_list[[length(df_list) + 1]] = h3("End?")
-
-        return(df_list)
+        return(result)
     })
 
-    # loads the default input text when the "load example" button is pressed
-    observeEvent(
-        input$load_example,
-        { updateTextInput(inputId = "interpro_input", value = DEFAULT_FASTA_TEXT) },
-        ignoreInit = TRUE
-    )
+    # output$runtime_output <- renderUI({
+    #     if (output$runtime_plot == FALSE) {
+    #         p(
+    #             "Error generating runtime plot, likely because only one sequence was supplied",
+    #             style="font-style: italic; text-align: center; padding: 20px;"
+    #         )
+    #     }
+    #     else {
+    #         plotOutput("runtime_plot")
+    #     }
+    # })
+
+    # set up load example buttons
+    observeEvent( input$load_single_example, {
+        file_data <- paste(readLines(single_fasta_example), collapse = "\n")
+        updateTextInput(inputId = "interpro_input", value = file_data)
+    }, ignoreInit = TRUE)
+    observeEvent( input$load_multi_example, {
+        file_data <- paste(readLines(multi_fasta_example), collapse = "\n")
+        updateTextInput(inputId = "interpro_input", value = file_data)
+    }, ignoreInit = TRUE)
 }
 
 
@@ -213,6 +228,10 @@ server <- function(input, output) {
 # =============================================================================
 
 console_stderr("* Application Started")
+
+# ignore RNG errors
+options(future.rng.onMisuse="ignore")
+options(error = function() traceback(3))
 
 # retain the stdout log
 if (!interactive()) sink(stderr(), type = "output")
